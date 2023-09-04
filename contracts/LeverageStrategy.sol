19887b0d7d10f615b575776f2bd754acc54fe43f
kei at <arseneeth@pm.me>
2023-09-04 13:41:30 +0200

    add addresses

--- contracts/LeverageStrategy.sol
+++ contracts/LeverageStrategy.sol
@@ -14,27 +14,34 @@ pragma solidity ^0.8.0;
 
 contract LeverageStrategy {
     // State variables
-    address public DAO;
-    address public wstETH;
-    address public crvUSD;
-    address public USDC;
 
-    // TODO write pools addresses
-    address public COILSUSDCBalancerPool;
-    address public COILSUSDCAuraPool;
+    // mainnet addresses
+    address public treasury; // recieves a fraction of yield
+    address public wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address public crvUSD = 0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E;
+    address public USDC   = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public COIL   = 0x823e1b82ce1dc147bbdb25a203f046afab1ce918;
+
+    // pools addresses
+    // https://etherscan.io/address/0x42fbd9f666aacc0026ca1b88c94259519e03dd67
+    address public COILSUSDCBalancerPool = 0x42FBD9F666AaCC0026ca1B88C94259519e03dd67;
+
+    // TODO: check if the booster is the right contract
+    // https://etherscan.io/address/0xa57b8d98dae62b26ec3bcc4a365338157060b234
+    address public AuraBooster = 0xA57b8d98dAE62B26Ec3bcC4a365338157060B234;
 
     // Events
     // Add relevant events to log important contract actions/events
 
     // Constructor
-    constructor(address _dao) {
-        DAO = _dao;
+    constructor(address _treasury) {
+        treasury = _treasury;
     }
 
-
     // Modifiers
-    modifier onlyDao() {
-        require(msg.sender == DAO,
+    // TODO: check if we need this solution
+    modifier onlyTreasury() {
+        require(msg.sender == Treasury,
             "Only the DAO can call this function.");
         _;
     }
