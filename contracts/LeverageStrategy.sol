bb91002f2f1526ceabf94886e04aca135767f9a1
kei at <arseneeth@pm.me>
2023-09-04 12:38:22 +0200

    upd strategy

--- contracts/LeverageStrategy.sol
+++ contracts/LeverageStrategy.sol
@@ -11,33 +11,80 @@ ____/ // /_/ /__  /_/ /  /   /  __/  / / / / /  __/  /_/ /_  ___ / /_/ /
 
 pragma solidity ^0.8.0;
 
-contract LeverageStrategy {
+import { StrategyBase } from ".StrategyBase.sol";
+
+contract LeverageStrategy is StrategyBase {
     // State variables
-    address public dao;
+    address public DAO;
     address public wstETH;
     address public crvUSD;
-    address public FRAX;
+    address public USDC;
 
     // TODO write pools addresses
+    address public COILSUSDCBalancerPool;
+    address public COILSUSDCAuraPool;
 
     // Events
     // Add relevant events to log important contract actions/events
 
+    // Constructor
+    constructor(address _dao) {
+        DAO = _dao;
+    }
+
+
     // Modifiers
     modifier onlyDao() {
-        require(msg.sender == owner,
+        require(msg.sender == DAO,
             "Only the DAO can call this function.");
         _;
     }
 
-    // Constructor
-    constructor(address _dao) {
-        dao = _dao;
-    }
-
     // TODO:
-    // Stake func
-    // Withdraw func
     // Collateral health monitor
 
+    function _invest(address, uint256[] calldata amounts, bytes calldata)
+        internal
+        override
+        returns (PositionReceipt memory receipt)
+    {
+
+        // Takes WSTETH
+
+        // Opens a position on crvUSD
+
+        // borrow crvUSD
+
+        // Exchange crvUSD to USDC on Curve
+
+        // Provide liquidity to the COIL/USDC Pool on Balancer
+
+        // Stake LP tokens on Aura Finance
+
+    }
+
+    function _claimRewards(address, bytes calldata) internal override {
+        // Claim rewards from Aura
+
+        // exchange for WSTETH
+
+        // call _invest
+    }
+
+    function _withdrawInvestment(address, uint256[] calldata amounts, bytes calldata extraStrategyData)
+        internal
+        override
+    {
+
+        // Exit Aura position
+
+        // Exit Balancer position
+
+        // Exchange everything to crvUSD
+
+        // repay debt
+
+        // withdraw colleteral
+
+    }
 }
