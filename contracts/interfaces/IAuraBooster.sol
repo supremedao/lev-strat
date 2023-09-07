5499a4eacbb993579b410cc972ed055de857baf7
kei at <arseneeth@pm.me>
2023-09-07 11:00:58 +0200

    add aura booster interface

--- contracts/interfaces/IAuraBooster.sol
+++ contracts/interfaces/IAuraBooster.sol
@@ -1,6 +1,13 @@
+// transaction example: https://etherscan.io/tx/0xb2d6067dfbde2eda99ae62b79d734b2943a78a06cba67ea63f48266b9a5f5138
+// contract: https://etherscan.io/address/0xa57b8d98dae62b26ec3bcc4a365338157060b234
+
 pragma solidity ^0.8.0;
 
 
 contract IAuraBooster {
-
+    function deposit(uint256 _pid, uint256 _amount, bool _stake) external returns(bool);
+    function depositAll(uint256 _pid, bool _stake) external returns(bool);
+    function withdraw(uint256 _pid, uint256 _amount) external returns(bool);
+    function withdrawAll(uint256 _pid) external returns(bool);
+    function withdrawTo(uint256 _pid, uint256 _amount, address _to) external returns(bool);
 }
