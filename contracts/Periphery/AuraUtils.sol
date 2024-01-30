e2912a0612a135fc29945c998ea75dd34dd53cbf
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-01-30 14:17:36 +0530

    docs: add natspec docs to AuraUtils contract

--- contracts/Periphery/AuraUtils.sol
+++ contracts/Periphery/AuraUtils.sol
@@ -15,39 +15,75 @@ import "../interfaces/IAuraBooster.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IBasicRewards.sol";
 
+/// @title Aura Utility Functions
+/// @author SupremeDAO
+/// @notice Provides utility functions for interacting with Aura Finance contracts.
+/// @dev This abstract contract includes functions for depositing, withdrawing, and unstaking in Aura Finance.
 abstract contract AuraUtils {
+
+    /// @notice The Pool ID for Aura Finance.
     uint256 public constant AURA_PID = 107;
-    //address of aura smart contract
+
+    /// @notice Address of the Aura Booster contract for deposit operations.
     IAuraBooster public constant AURA_BOOSTER = IAuraBooster(0xA57b8d98dAE62B26Ec3bcC4a365338157060B234);
+
+    /// @notice Address of the Aura Vault for staking operations.
     IBasicRewards public constant AURA_VAULT = IBasicRewards(0xe39570EF26fB9A562bf26F8c708b7433F65050af);
 
+    /// @notice Determines the token to be staked.
+    /// @dev Should be overridden to return the specific token to stake in Aura.
+    /// @return The IERC20 token to be staked.
     function _tokenToStake() internal view virtual returns (IERC20);
 
+    /// @notice Deposits all available tokens into the Aura Booster.
+    /// @dev Approves and then deposits all tokens held by this contract into Aura using the depositAll method.
     function _depositAllAura() internal {
+        // Approve the Aura Booster to spend the token
         require(
-            _tokenToStake().approve(address(AURA_BOOSTER), _tokenToStake().balanceOf(address(this))), "Approval failed"
+            _tokenToStake().approve(address(AURA_BOOSTER), _tokenToStake().balanceOf(address(this))),
+            "Approval failed"
         );
+        // Deposit all tokens to Aura
         require(AURA_BOOSTER.depositAll(AURA_PID, true));
     }
 
-    function _depositAura(uint256 ammount) internal {
-        require(_tokenToStake().approve(address(AURA_BOOSTER), ammount), "Approval failed");
-        require(AURA_BOOSTER.deposit(AURA_PID, ammount, true));
+    /// @notice Deposits a specific amount of tokens into the Aura Booster.
+    /// @dev Approves and then deposits a specified amount of tokens into Aura.
+    /// @param amount The amount of tokens to deposit.
+    function _depositAura(uint256 amount) internal {
+        // Approve the Aura Booster to spend the token
+        require(_tokenToStake().approve(address(AURA_BOOSTER), amount), "Approval failed");
+        // Deposit specified amount of tokens to Aura
+        require(AURA_BOOSTER.deposit(AURA_PID, amount, true));
     }
 
+    /// @notice Withdraws all tokens from the Aura Booster.
+    /// @dev Withdraws all staked tokens from Aura.
     function _withdrawAllAura() internal {
+        // Withdraw all staked tokens from Aura
         AURA_BOOSTER.withdrawAll(AURA_PID);
     }
 
-    function _withdrawAura(uint256 ammount) internal {
-        AURA_BOOSTER.withdraw(AURA_PID, ammount);
+    /// @notice Withdraws a specific amount of tokens from the Aura Booster.
+    /// @dev Withdraws a specified amount of staked tokens from Aura.
+    /// @param amount The amount of tokens to withdraw.
+    function _withdrawAura(uint256 amount) internal {
+        // Withdraw specified amount of staked tokens from Aura
+        AURA_BOOSTER.withdraw(AURA_PID, amount);
     }
 
+    /// @notice Unstakes and withdraws a specific amount of tokens from the Aura Vault.
+    /// @dev Unstakes and withdraws a specified amount of tokens from Aura, including accrued rewards.
+    /// @param amount The amount of tokens to unstake and withdraw.
     function _unstakeAndWithdrawAura(uint256 amount) internal {
+        // Unstake specified amount and withdraw from Aura Vault
         AURA_VAULT.withdrawAndUnwrap(amount, true);
     }
 
+    /// @notice Unstakes and withdraws all tokens from the Aura Vault.
+    /// @dev Unstakes and withdraws all tokens from Aura, including accrued rewards.
     function _unstakeAllAndWithdrawAura() internal {
+        // Unstake all tokens and withdraw from Aura Vault
         AURA_VAULT.withdrawAllAndUnwrap(true);
     }
 }
